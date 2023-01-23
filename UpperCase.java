87d56355ed02aa892c7bb60df1e90a3609a095b6
NarikellapuHaritha <119496343+NarikellapuHaritha@users.noreply.github.com>
2023-01-23 19:25:44 +0530

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,18 @@
+package javaassignment;
+import java.util.Scanner;
+public class UpperCase {
+
+	 	public static void main(String[]args) {
+	 		Scanner in =new Scanner(System.in);
+	 		System.out.println("Input a String:");
+	 		String line=in.nextLine();
+	 		line=line.toUpperCase();
+	 		System.out.println(line);
+	 		
+	 	}
+	 	
+
+	 }
+
+
+
