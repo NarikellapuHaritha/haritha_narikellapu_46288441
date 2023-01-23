64f1a966e2c064aec1a9e1267e8af845450d1143
NarikellapuHaritha <119496343+NarikellapuHaritha@users.noreply.github.com>
2023-01-23 19:31:14 +0530

    Add files via upload

---
+++ presistance.java
@@ -0,0 +1,21 @@
+package javaassignment;
+
+public class presistance {
+	public static void main(String args[]) {
+		
+	}
+
+}
+abstract class presistance1{
+	void persist() {
+		System.out.println("This is persistance");
+	}}
+class Filepresistance{
+	void persist() {
+		System.out.println("this is File persistance");
+	}}
+abstract class Databasepresistance{
+	void persist() {
+		System.out.println("This is Database persistance");
+		
+	}}
